74cf7e658aeffe5bb67fdd0ec2726df75716d2e2
Cris Liao <clia@163.com>
2018-10-19 21:45:46 +0800

    make compile with rustc 1.31.0-nightly

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -8,21 +8,21 @@ extern crate rustc_plugin;
 
 use rustc_plugin::Registry;
 use syntax::ext::base::{MultiDecorator, MultiModifier};
-use syntax::parse::token::intern;
+use syntax::symbol::Symbol;
 use rustc::hir::map::blocks::MaybeFnLike;
 
 use syntax::ext::base::{ExtCtxt, Annotatable};
-use syntax::codemap::Span;
+use syntax::source_map::Span;
 use syntax::ptr::P;
 
 use syntax::ast::{Item, MetaItem};
 use syntax::attr;
 use syntax_ext::deriving::generic::{combine_substructure, EnumMatching, FieldInfo, MethodDef, Struct, Substructure, TraitDef, ty};
-use syntax::parse::token::InternedString;
+use syntax::source_map::symbol::Ident;
 
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
-    reg.register_syntax_extension(intern("pg_export"), MultiModifier(box expand_pg_export));
+    reg.register_syntax_extension(Symbol::intern("pg_export"), MultiModifier(box expand_pg_export));
 }
 
 pub fn expand_pg_export(cx: &mut ExtCtxt, span: Span, _: &MetaItem, item: Annotatable) -> Annotatable {
@@ -35,18 +35,33 @@ pub fn expand_pg_export(cx: &mut ExtCtxt, span: Span, _: &MetaItem, item: Annota
     match item {
         Annotatable::Item(it) => {
             let mut new_it = (*it).clone();
-            new_it.attrs.push(attr::mk_attr_outer(attr::mk_attr_id(), attr::mk_word_item(InternedString::new("no_mangle"))));
+            new_it.attrs.push(attr::mk_attr_outer(span, attr::mk_attr_id(), attr::mk_word_item(Ident::from_str("no_mangle"))));
             Annotatable::Item(P(new_it))
         }
         Annotatable::ImplItem(it) => {
             let mut new_it = (*it).clone();
-            new_it.attrs.push(attr::mk_attr_outer(attr::mk_attr_id(), attr::mk_word_item(InternedString::new("no_mangle"))));
+            new_it.attrs.push(attr::mk_attr_outer(span, attr::mk_attr_id(), attr::mk_word_item(Ident::from_str("no_mangle"))));
             Annotatable::ImplItem(P(new_it))
         }
         Annotatable::TraitItem(tt) => {
             let mut new_it = (*tt).clone();
-            new_it.attrs.push(attr::mk_attr_outer(attr::mk_attr_id(), attr::mk_word_item(InternedString::new("no_mangle"))));
+            new_it.attrs.push(attr::mk_attr_outer(span, attr::mk_attr_id(), attr::mk_word_item(Ident::from_str("no_mangle"))));
             Annotatable::TraitItem(P(new_it))
+        }
+        Annotatable::ForeignItem(tt) => {
+            let mut new_it = (*tt).clone();
+            new_it.attrs.push(attr::mk_attr_outer(span, attr::mk_attr_id(), attr::mk_word_item(Ident::from_str("no_mangle"))));
+            Annotatable::ForeignItem(P(new_it))
+        }
+        Annotatable::Stmt(tt) => {
+            let mut new_it = (*tt).clone();
+            //new_it.attrs.push(attr::mk_attr_outer(span, attr::mk_attr_id(), attr::mk_word_item(Ident::from_str("no_mangle"))));
+            Annotatable::Stmt(P(new_it))
+        }
+        Annotatable::Expr(tt) => {
+            let mut new_it = (*tt).clone();
+            //new_it.attrs.push(attr::mk_attr_outer(span, attr::mk_attr_id(), attr::mk_word_item(Ident::from_str("no_mangle"))));
+            Annotatable::Expr(P(new_it))
         }
     }
 
